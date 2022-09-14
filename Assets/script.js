a9206e53acc3490336df6e6193739558dc42b86a
nhartm <natalie.hartmann98@gmail.com>
2022-09-14 15:58:14 -0700

    added startGame function

---
+++ Assets/script.js
@@ -0,0 +1,24 @@
+// Init function is called when page loads
+function init() {
+    setPasses();
+    setFails();
+}
+
+// Start button and next button are activated
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+// startGame function is called when the start button is clicked
+function startGame(){
+isPass = false;
+timerCount = 30;
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+startTimer()
+}
